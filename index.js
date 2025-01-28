65864f2157db648650e7ba738179952183e141df
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2025-01-28 10:39:04 +0530

    age distribution Calculations added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const getRowsFromRawCsv = (rawFileData) => {
-    const allRows = rawFileData.replace('\r','').split('\n');
+    const allRows = rawFileData.replace('\r', '').split('\n');
     return allRows
 }
 
@@ -36,7 +36,7 @@ const extractJsonDataFromRows = (allRows) => {
             let previousRef = eachDataObj;
             propertyTrain?.forEach((eachTrainCoach, trIndex) => {
                 if (trIndex === propertyTrain.length - 1) {
-                    previousRef[eachTrainCoach] = eachData.replace('\r','');
+                    previousRef[eachTrainCoach] = eachData.replace('\r', '');
                 } else {
                     previousRef[eachTrainCoach] = previousRef?.[eachTrainCoach] ?? {}
                     previousRef = previousRef[eachTrainCoach];
@@ -49,27 +49,66 @@ const extractJsonDataFromRows = (allRows) => {
     return jsonDataArray
 }
 
-const insertDataIntoDb = (jsonDataArray, res) => {
-    jsonDataArray.forEach((jsonData) => {
-        const additionalInfo = _.omit(jsonData, ['name', 'age', 'address'])
-        
-        pool.query(
-            'INSERT INTO users (name, age, address, additional_info, id) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-            [
-                `${jsonData?.name.firstname} ${jsonData?.name.lasttname}`,
-                Number(jsonData?.age),
-                jsonData?.address,
-                additionalInfo,
-                _.uniqueId(Math.round(Math.random() * 100000))
-            ],
-            (error) => {
-                if (error) {
-                    res.status(500)
-                    throw error
-                }
-            })
-    })
+const resultCallback = (res) => (ageDistribution) => {
+    console.log('Age Distribution');
+    console.table(ageDistribution);
+    res.send({ ageDistribution })
 }
+
+const insertDataIntoDb = (jsonDataArray, res) => {
+    let queryFailure = false;
+    jsonDataArray.forEach((jsonData) => {
+        if (!queryFailure) {
+            const additionalInfo = _.omit(jsonData, ['name', 'age', 'address'])
+
+            pool.query(
+                'INSERT INTO users (name, age, address, additional_info, id) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+                [
+                    `${jsonData?.name.firstname} ${jsonData?.name.lasttname}`,
+                    Number(jsonData?.age),
+                    jsonData?.address,
+                    additionalInfo,
+                    _.uniqueId(Math.round(Math.random() * 100000))
+                ],
+                (error) => {
+                    if (error) {
+                        queryFailure = error
+                    }
+                })
+        }
+    })
+    if (!queryFailure) {
+        getAgeDistribution(resultCallback(res));
+        return;
+    }
+    res.status(500).send({ msg: "Failed", queryFailure })
+}
+
+const getAgeDistribution = (callbackR) => {
+    pool.query(
+        `SELECT
+            CASE
+                WHEN age < 20 THEN '< 20'
+                WHEN age BETWEEN 20 AND 39 THEN '20-40'
+                WHEN age BETWEEN 40 AND 60 THEN '40-60'
+                ELSE '> 60'
+            END AS age_group,
+            COUNT(*) AS total_users
+        FROM users
+        GROUP BY
+            CASE
+                WHEN age < 20 THEN '< 20'
+                WHEN age BETWEEN 20 AND 39 THEN '20-40'
+                WHEN age BETWEEN 40 AND 60 THEN '40-60'
+                ELSE '> 60'
+            END`,
+        [],
+        (e, r) => {
+            callbackR(r.rows);
+        }
+    )
+}
+
 
 app.post('/api/parseCsvToJson', upload.array('fileData'), (req, res) => {
     let fileData = fs.createReadStream(req.files[0].path, { encoding: 'utf8', autoClose: true });
@@ -83,10 +122,7 @@ app.post('/api/parseCsvToJson', upload.array('fileData'), (req, res) => {
     fileData.on('close', () => {
         const allRows = getRowsFromRawCsv(fileChunk)
         jsonDataArray = extractJsonDataFromRows(allRows);
-
         insertDataIntoDb(jsonDataArray, res)
-        res.send({ jsonDataArray })
-
     })
 })
 
@@ -103,14 +139,17 @@ app.post('/api/csvToJsonRaw', (req, res) => {
         const fileContents = fileBufferString.split(`${boundary}`)[1];
         const rawFileData = fileContents.split('\r\n\r\n')[1]
         const rawCsvData = rawFileData.slice(0, rawFileData.lastIndexOf('\r\n'));
-
         const allRows = getRowsFromRawCsv(rawCsvData)
 
         jsonDataArray = extractJsonDataFromRows(allRows);
+        insertDataIntoDb(jsonDataArray, res);
 
-        insertDataIntoDb(jsonDataArray, res)
-        res.send({ jsonDataArray })
     });
+})
+
+
+app.get('/api/getAgeDistribution', (req, res) => {
+    getAgeDistribution(resultCallback(res));
 })
 
 app.listen(process.env.PORT, () => {
